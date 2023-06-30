a4ecd8ee658379e4ad15f29facc10d3b66da0a2f
Yusuf Ayvaz <65962835+YusufAyvaz202@users.noreply.github.com>
2023-06-30 16:02:11 +0300

    Boomerangta sorun kalmadı. Düzgün bir şekilde zarar veriyor
    Animasyonlar eklenecek.

--- Assets/TakeDamage.cs
+++ Assets/TakeDamage.cs
@@ -7,55 +7,68 @@ public class TakeDamage : MonoBehaviour
 {
 
     private Mover mover;
+    private Attack attack;
     public Boomerang boomerang;
 
     public int health = 100;
     public bool isFrozen = false;
+    private bool isPlayerFrozen = false;
+
     private void Start()
     {
         mover = GetComponent<Mover>();
+        attack = GetComponent<Attack>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Boomerang") && boomerang.isReturning==false)
+        if (other.CompareTag("Boomerang") && boomerang.isReturning == false)
         {
-            health = -5;
+            health -= 5;
             Debug.Log("oldu1");
-
         }
-        if (other.CompareTag("FrozenBoomerang") && boomerang.isReturning==false)
+        if (other.CompareTag("FrozenBoomerang") && boomerang.isReturning == false)
         {
-            health = -10;
+            health -= 10;
             isFrozen = true;
             mover.enabled = false;
+            attack.enabled = false;
             Debug.Log("oldu2");
             StartCoroutine(ExitFreeze());
         }
     }
 
+    private IEnumerator ExitFreeze()
+    {
+        yield return new WaitForSeconds(3f); // Donma süresi burada 3 saniye olarak varsayýlmýþtýr, ihtiyaca göre deðiþtirilebilir
+
+        isFrozen = false;
+        mover.enabled = true;
+        attack.enabled = true;
+    }
+
     //private void OnCollisionEnter(Collision collision)
     //{
-    //    if (collision.collider.CompareTag("Boomerang")&& !boomerang.isReturning)
+    //    if (collision.collider.CompareTag("FrozenBoomerang") && isFrozen)
     //    {
-    //        health = -5;
-    //        Debug.Log("oldu1");
-
-    //    }
-    //    if (collision.collider.CompareTag("FrozenBoomerang")&& !boomerang.isReturning)
-    //    {
-    //        health = -10;
-    //        isFrozen = true;
-    //        mover.enabled = false;
-    //        Debug.Log("oldu2");
-    //        StartCoroutine(ExitFreeze());
+    //        isPlayerFrozen = true;
+    //        StartCoroutine(ExitPlayerFreeze());
     //    }
     //}
 
+    //private IEnumerator ExitPlayerFreeze()
+    //{
+    //    yield return new WaitForSeconds(2f); // Oyuncunun donma süresi burada 2 saniye olarak varsayýlmýþtýr, ihtiyaca göre deðiþtirilebilir
 
-    IEnumerator ExitFreeze()
-    {
-        yield return new WaitForSeconds(3f);
-        mover.enabled = true;
-    }
+    //    isPlayerFrozen = false;
+    //}
+
+    //private void Update()
+    //{
+    //    if (!isPlayerFrozen)
+    //    {
+    //        // Oyuncunun hareket kodlarý buraya gelecek
+    //    }
+    //}
+
 }
